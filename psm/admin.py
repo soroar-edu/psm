dc2cb4c9da21e3cc60924ab5a848e62087c35683
Rayhan <rayhancse08@gmail.com>
2021-08-12 09:51:02 +0600

    first commit

---
+++ psm/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from .models import Category, District, DistrictUserPermission, Item, Notice, UserProfile, DistrictStore
+
+# from api.admin import GenericModelAdmin
+from django.contrib import admin
+
+
+# from api import models
+
+
+class ItemInline(admin.TabularInline):
+    model = Item
+    # form = OrderFlavorInlineForm
+    extra = 1
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (ItemInline,)
+    list_display = (
+        'name', 'total_item')
+    search_fields = ('name',)
+
+
+class DistrictStoreInline(admin.TabularInline):
+    model = DistrictStore
+    # form = OrderFlavorInlineForm
+    extra = 1
+
+
+class DistrictAdmin(admin.ModelAdmin):
+    inlines = (DistrictStoreInline,)
+    list_display = (
+        'name',)
+    search_fields = ('name',)
+
+
+# Register your models here.
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(District, DistrictAdmin)
+admin.site.register(DistrictUserPermission)
+admin.site.register(Item)
+admin.site.register(Notice)
+admin.site.register(UserProfile)
---
+++ psm/models.py
@@ -0,0 +1,88 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=120)
+    total_item = models.IntegerField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.total_item = self.get_total_item()
+
+        super(Category, self).save(*args, **kwargs)
+
+    def get_total_item(self):
+        return self.items.count()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=120)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
+    stock_quantity = models.IntegerField(null=True, blank=True)
+    used_quantity = models.IntegerField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Notice(models.Model):
+    title = models.CharField(max_length=120)
+    body = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=255, null=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class District(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class DistrictUserPermission(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='district_user_permissions', null=True,
+                             blank=True)
+    district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='district_user_permissions',
+                                 null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.district.name
+
+
+class DistrictStore(models.Model):
+    district = models.ForeignKey(District, on_delete=models.CASCADE, related_name='district_stores')
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='district_stores')
+    stock_quantity = models.IntegerField(null=True, blank=True)
+    used_quantity = models.IntegerField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.district.name
